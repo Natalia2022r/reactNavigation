bbf2e41c2bc9e4672ea9a6aef08c8a06c1529549
Natalia2022r <99670282+Natalia2022r@users.noreply.github.com>
2022-07-30 22:12:45 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { HashRouter } from "react-router-dom";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Contacts from "./component/contacts/Contacts";
 import  HomePage  from "./component/HomePage"
@@ -9,14 +10,15 @@ import User from "./component/contacts/user";
 function App() {
   return (
     <div className="App">
-    <Router>
+    
+    <HashRouter>
     <Navigation/>
       <Route path= "/" exact component = {HomePage}/>
       <Route path= "/public" component = {Public}/>
       <Route path= "/photo" component = {Photo}/>
       <Route path= "/contacts" exact component = {Contacts}/>
       <Route path= "/contacts/:id" component = {User}/>
-    </Router>
+      </HashRouter>
     </div>
   );
 }
